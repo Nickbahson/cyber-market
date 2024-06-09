f000c424afa22f08ae1fb94893c1f9f5c20e67ed
Nicholas Babu <nijoba33@gmail.com>
2024-06-09 08:00:27 +0300

    added template scaffoldings

---
+++ main.js
@@ -0,0 +1,9 @@
+(async () => {
+    const menuItems = document.querySelectorAll('#main-menu li')
+
+    menuItems.forEach( menuItem => {
+        menuItem.addEventListener('click', () => {
+            alert('You clicked me, please link me to a page so I can display it\'s contents instead!')
+        })
+    })
+})()
